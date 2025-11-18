6f010ea34fe35880265247005586b4454a1243ba
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-18 06:53:18 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-18

--- Herodotus/Views/MainWindow.xaml.cs
+++ Herodotus/Views/MainWindow.xaml.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 ноября 2025 06:53:16
- * Version: 1.0.19
+ * Last Updated: 18 ноября 2025 06:53:16
+ * Version: 1.0.20
  */
 
 using System.Windows;
